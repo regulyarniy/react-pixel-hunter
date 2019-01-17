8d4c5805f22471f867264a2579bcc08577a90af8
Regulyarniy Nikolay <regularniy@gmail.com>
2019-01-17 16:16:06 +1000

    adding react to eslint config

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(<App />, document.getElementById(`main`));
